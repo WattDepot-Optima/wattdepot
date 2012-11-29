15a586c0283083e98b9a553fbec0daa91e94abea
Jordan Do <jordantado@gmail.com>
2012-11-28 23:51:55 -1000

    Fixed some checkstyle errors and added documentation

--- src/main/java/org/wattdepot/benchmark/Parser.java
+++ src/main/java/org/wattdepot/benchmark/Parser.java
@@ -1,4 +1,5 @@
 package org.wattdepot.benchmark;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import org.apache.batik.dom.GenericDOMImplementation;
@@ -21,46 +22,77 @@ import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.util.Hashtable;
- 
+
+/**
+ * The front end for the WattDepot benchmarking suite.
+ * Reads a file called config.xml from default location
+ * user.home in System Properties, a different location
+ * can be supplied in a command line argument.
+ *
+ * Parser reads the config file and generates the tests
+ * and resulting graphs.
+ *
+ * Graphing methods created by Greg.
+ *
+ * @author Jordan Do
+ * @author Greg Burgess
+ */
+
 public class Parser {
- 
-  public static void main(String argv[]) {
- 
+  
+  /** The default location for config.xml. **/
+  public static String fileLoc = System.getProperty("user.home");
+  
+  /** The main driver for the benchmarking.
+   *  Parses the file and runs the tests.
+   *  @param args command line argument that can
+   *  contain the location of the config file.
+   */
+  public static void main(final String args[]) {
     try {
-      File fXmlFile = new File("c:\\file.xml");
+      if (args.length > 0) {
+        fileLoc = args[0];
+      }
+      File fXmlFile = new File(fileLoc + "\\config.xml");
       DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
       DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
       Document doc = dBuilder.parse(fXmlFile);
       doc.getDocumentElement().normalize();
-   
-      NodeList listOfTests = doc.getElementsByTagName("test"); //create list of all tests
-   
+      //create a list of all test cases
+      NodeList listOfTests = doc.getElementsByTagName("test");
       for (int i = 0; i < listOfTests.getLength(); i++) {
         Node test = listOfTests.item(i); //look at an individual test
         NodeList tags = test.getChildNodes(); //list of all params for that test
         Hashtable<String, String> params = new Hashtable<String, String>();
-        for (int k = 0;k < tags.getLength(); k++) {
+        for (int k = 0; k < tags.getLength(); k++) {
           if (tags.item(k).getNodeType() == Node.ELEMENT_NODE) {
             String key = tags.item(k).getNodeName(); //name of the param
-            String value = tags.item(k).getFirstChild().getNodeValue(); //value of param
+            //tags should be at a depth of one. so the first child is the value
+            String value = tags.item(k).getFirstChild().getNodeValue();
             params.put(key, value);
-          }  
+          }
         }
-        String dir = System.getProperty("user.home") 
+        String dir = System.getProperty("user.home")
         + "\\benchmark\\" + params.get("name") + "\\";
         params.put("dir", dir);
         runTest(params);
       }
-    } catch (Exception e) {
-    e.printStackTrace();
+    } 
+    catch (Exception e) {
+      e.printStackTrace();
     }
   }
-  
-  private static void runTest(Hashtable<String, String> params) {
-
+  /**
+   * Runs the test and outputs a graph and text files containing
+   * results.
+   * @param params Hashtable<String, String> containing all
+   * user modifiable configurations.
+   */
+  private static void runTest(final Hashtable<String, String> params) {
     try { //check to make sure name entered is valid
       WattDepotEnum command = WattDepotEnum.valueOf(params.get("name"));
-      }
+      System.out.println("Running benchmark for " + command);
+    }
     catch (IllegalArgumentException e) {
       System.err.println("Invalid Command \"" + params.get("name") + "\".");
       System.exit(1);
@@ -72,29 +104,36 @@ public class Parser {
     try {
       WattDepotBenchmark run;
       //check how many tests to run
-      if (params.containsKey("min") || 
-          params.containsKey("max") || 
-          params.containsKey("interval")) {
+      if (params.containsKey("min")
+          || params.containsKey("max")
+          || params.containsKey("interval")) {
         if (params.contains("range")) {
           //there's both range and min/max
-          System.out.println("Parameters must include a user defined range of threads " +
-          		"or a min, max, and interval. (enter a single number as the range for one" +
-          		" test only)");
+          System.out.println("Parameters must include a user defined range of "
+              + "threads or a min, max, and interval. (enter a single "
+              + "number as the range for one test only)");
           System.exit(1);
         }
-        else if (params.containsKey("min") && 
-            params.containsKey("max") && 
-            params.containsKey("interval")) {
-          //we're good to go
+        else if (params.containsKey("min")
+            && params.containsKey("max")
+            && params.containsKey("interval")) {
           int min = Integer.valueOf(params.get("min"));
           int max = Integer.valueOf(params.get("max"));
           int interval = Integer.valueOf(params.get("interval"));
-          for (int i = min; i <= max; i+= interval) {
+          //check to make sure numbers are valid
+          if (max < min || interval < 1) {
+            System.out.println("Max must be greater than"
+              + " min and the interval must be 1 or greater");
+            System.exit(1);
+          }
+              //we're good to go
+          for (int i = min; i <= max; i += interval) {
             params.put("numThreads", String.valueOf(i));
             run = new WattDepotBenchmark(params.get("name"), params);
             run.run();
             //store results for graphing
-            long requests = run.getFinalResults().getResults().get("requestCount");
+            long requests;
+            requests = run.getFinalResults().getResults().get("requestCount");
             long errors = run.getFinalResults().getResults().get("errorCount");
             successSeries.add(i, requests);
             errorSeries.add(i, errors);
@@ -115,14 +154,15 @@ public class Parser {
       }
       else if (params.containsKey("range")) {
         //no min/max but range is defined
-        if(params.get("range").contains(",")) { //if range is a csv
+        if (params.get("range").contains(",")) { //if range is a csv
           String[] numbers = params.get("range").split(",");
           for (int i = 0; i < numbers.length; i++) {
             params.put("numThreads", numbers[i]);
             run = new WattDepotBenchmark(params.get("name"), params);
             run.run();
             //store results for graphing
-            long requests = run.getFinalResults().getResults().get("requestCount");
+            long requests;
+            requests = run.getFinalResults().getResults().get("requestCount");
             long errors = run.getFinalResults().getResults().get("errorCount");
             int threads = Integer.valueOf(numbers[i]);
             successSeries.add(threads, requests);
@@ -136,7 +176,7 @@ public class Parser {
           String name = params.get("name") + "_" + "USER_DEFINED_AGGREGATE";
           createSVG(chart1, name, params.get("dir"));
         }
-        else {//if range is a single number
+        else { //if range is a single number
         params.put("numThreads", String.valueOf(params.get("range")));
         run = new WattDepotBenchmark(params.get("name"), params);
         run.run();
@@ -145,7 +185,7 @@ public class Parser {
       else {
         //neither range nor min/max is defined
         run = new WattDepotBenchmark(params.get("name"), params);
-        run.run(); 
+        run.run();
       }
     }
     catch (Exception e) {
@@ -183,7 +223,9 @@ public class Parser {
    * @param name File name of the chart.
    * @param dir String directory to put chart in
    */
-  public static void createSVG(final JFreeChart chart, final String name, final String dir) {
+  public static void createSVG(final JFreeChart chart,
+                               final String name,
+                               final String dir) {
     // THE FOLLOWING CODE BASED ON THE EXAMPLE IN THE BATIK DOCUMENTATION...
     // Get a DOMImplementation
     DOMImplementation domImpl
